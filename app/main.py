d30cfd8afcff799bcd7e588425d56183da96f040
Mark Bonicillo <mark.a.bonicillo@gmail.com>
2024-08-28 08:19:41 -0700

    Minor fixes

--- app/main.py
+++ app/main.py
@@ -1,17 +1,18 @@
 from fastapi import FastAPI
-from app.routers import intake
 from fastapi.middleware.cors import CORSMiddleware
+
+from app.clients.router import router as clients_router
+
 
 app = FastAPI()
 
 # Set API endpoints on router
-app.include_router(intake.router)
+app.include_router(clients_router)
 
 # Configure CORS middleware
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Allows all origins
-    allow_credentials=True,
     allow_methods=["*"],  # Allows all methods, including OPTIONS
     allow_headers=["*"],  # Allows all headers
 )
